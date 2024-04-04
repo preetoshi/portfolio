a4cc720eb1a900c6169e3dac179ac281c958420f
Preet Dalal <dalalpreet@gmail.com>
2024-04-05 03:45:14 +0530

    Added root handler so its withini apip folder

--- api/server.js
+++ api/server.js
@@ -2,11 +2,10 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 2000;
 
-app.get('/api', (req, res) => {
+app.get('/', (req, res) => {
     res.json({ message: 'Welcome to my chatbot API!' });
 });
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
-    
 });
